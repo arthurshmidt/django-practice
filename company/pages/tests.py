f534fa8be6eed3a3f378ec1d058415e5469a01c5
arthurshmidt <arthur@arthurshmidt.com>
2024-10-09 12:55:43 -0400

    continued on test programming. nxt->Chp4-testing

--- company/pages/tests.py
+++ company/pages/tests.py
@@ -1,5 +1,6 @@
 # pages/tests.py
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -8,7 +9,15 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
 class AboutpageTests(SimpleTestCase):
-    def test_ult_exists_at_correct_location(self):
+    def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
