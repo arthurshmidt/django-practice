728acbf61db7e5a7449219f78b190e08080085a1
arthurshmidt <arthur@arthurshmidt.com>
2024-10-23 15:21:31 -0400

    Completed Messag Board List view. Nxt->Ch5.Tests

--- message-board/posts/urls.py
+++ message-board/posts/urls.py
@@ -1,7 +1,9 @@
 # posts/urls.py
 from django.urls import path
-from .views import post_list
+# from .views import post_list
+from .views import PostList
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
+#    path("", post_list, name="post_list"),
+    path("", PostList.as_view(), name="home"),
 ]
